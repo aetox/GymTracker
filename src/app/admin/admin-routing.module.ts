14467ef15d019428b7cabc8b4075826142bb4197
aetox <core.editor>
2023-05-23 21:12:34 +0200

    Rootage des pages admin

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,33 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlayoutComponent } from './alayout/alayout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+      path:'',
+      component: AlayoutComponent, children : [
+        {
+          //Redirection vers dashboard lorsque on arrive dans admin. pathMatc:'full' = liens complet 
+          path:'', redirectTo: 'dashboard', pathMatch:'full'
+        },
+        {
+          path:'dashboard',
+          component: DashboardComponent
+        },
+        {
+          // permet d'importer les elements du module Users
+          path:'users', loadChildren: () => import('./users/users.module')
+          .then(m=>m.UsersModule)
+        },
+        {
+          path:'exercices', loadChildren: () => import('./exercices/exercices.module')
+          .then(m=>m.ExercicesModule)
+        }
+      ]
+    
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/admin/exercices/exercices-routing.module.ts
+++ src/app/admin/exercices/exercices-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EIndexComponent } from './e-index/e-index.component';
+import { EEditComponent } from './e-edit/e-edit.component';
+import { EAddComponent } from './e-add/e-add.component';
+import { EDeleteComponent } from './e-delete/e-delete.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: EIndexComponent
+  },
+  {
+    // /:id => Permet d'ajouter un lien dynamique avec comme param : id
+    path:'edit/:id',
+    component: EEditComponent
+  },
+  {
+    path:'add',
+    component: EAddComponent
+  },
+  {
+    path:'delete/:id',
+    component: EDeleteComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
